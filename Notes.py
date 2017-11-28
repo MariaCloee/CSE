0ff8b2796fe0b00a616553c01d2160d72e57a2d2
Maria Garcia <536y@wetigers.org>
2017-11-28 15:41:12 -0800

    Add Functions

--- Notes.py
+++ Notes.py
@@ -1,31 +1,59 @@
-print("Hello World")
+# print("Hello World")
+#
+# # Maria Garcia
+#
+# print(3+5)
+# print(5-3)
+# print(5*3)
+# print(6/2)
+# print(3**2)   # 2 ** means to the power of and 1 * means multiply
+#
+# print()   # Creates a blank line
+# print("See if you can figure this out")
+# print(100%3)
+#
+# # Variable
+# car_name =  "Wiebe Mobile"
+# car_type= "Lamborghini Sesto Elemento"
+# car_cylinders = 8
+# car_mpg = 9000.1
+#
+# # Inline Printing
+# print("My car is the %s" % car_name)  # %s = insert and  %d = number
+# print("My car is the %s. It is a %s." % (car_name,car_type))
+#
+# # Taking input
+# name = input("What is your name?")
+# print("Hello %s." % name)
+# # print(name)
+#
+# age = input("What is your age?")
+# print("You are %s years old." % age)
+#
+# # Change the file
 
-# Maria Garcia
 
-print(3+5)
-print(5-3)
-print(5*3)
-print(6/2)
-print(3**2)   # 2 ** means to the power of and 1 * means multiply
+def print_hw():
+    print("Hello World")
+    # 4 spaces = 1 indent (very important)
 
-print()   # Creates a blank line
-print("See if you can figure this out")
-print(100%3)
 
-# Variable
-car_name =  "Wiebe Mobile"
-car_type= "Lamborghini Sesto Elemento"
-car_cylinders = 8
-car_mpg = 9000.1
+print_hw()
 
-# Inline Printing
-print("My car is the %s" % car_name)  # %s = insert and  %d = number
-print("My car is the %s. It is a %s." % (car_name,car_type))
 
-# Taking input
-name = input("What is your name?")
-print("Hello %s." % name)
-# print(name)
+def say_hi(name):  # name is a "parameter"
+    print("Hello %s." % name)
+    print("I hope you have a fantastic day!")
 
-age = input("What is your age?")
-print("You are %s years old." % age)
+
+say_hi("John")
+
+
+def birthday(age):
+    age += 1   # age = age + 1
+    print(age)
+
+
+say_hi("John")
+print("John is 15. Next year:")
+birthday(15)
