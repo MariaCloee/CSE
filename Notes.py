753a5d74f01d6acbfef7fcddf4566b5d6f41676c
Maria Garcia <536y@wetigers.org>
2017-11-28 15:13:30 -0800

    Added Previous Notes

---
+++ Notes.py
@@ -0,0 +1,31 @@
+print("Hello World")
+
+# Maria Garcia
+
+print(3+5)
+print(5-3)
+print(5*3)
+print(6/2)
+print(3**2)   # 2 ** means to the power of and 1 * means multiply
+
+print()   # Creates a blank line
+print("See if you can figure this out")
+print(100%3)
+
+# Variable
+car_name =  "Wiebe Mobile"
+car_type= "Lamborghini Sesto Elemento"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline Printing
+print("My car is the %s" % car_name)  # %s = insert and  %d = number
+print("My car is the %s. It is a %s." % (car_name,car_type))
+
+# Taking input
+name = input("What is your name?")
+print("Hello %s." % name)
+# print(name)
+
+age = input("What is your age?")
+print("You are %s years old." % age)
