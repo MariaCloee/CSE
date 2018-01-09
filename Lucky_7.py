b4c6e2ab1d498f6613204002925cfbf2c202e235
Maria <31050501+MariaCloee@users.noreply.github.com>
2018-01-09 15:42:30 -0800

    Added to Lucky Seven
    1/9/18

--- Lucky_7.py
+++ Lucky_7.py
@@ -4,6 +4,8 @@ Money = 15
 Bet = 1
 lucky_seven = 7
 rounds = 0
+MaxMoney = 95
+MaxMoneyRound = 20
 while Money > 0:
     print("This is your money right now: %s." % Money)
 
@@ -12,16 +14,15 @@ while Money > 0:
     print(Dice1, Dice2)
 
     if Dice1 + Dice2 == lucky_seven:
-        Money += 4
+        Money += 5
     elif Dice1 + Dice2 > lucky_seven:
         Money -= 1
     elif Dice1 + Dice2 < lucky_seven:
         Money -= 1
     print("You now have $" + str(Money) + " left")
     rounds += 1
+
     if Money == 0:
         print("This is how many rounds you have played: %s." % rounds)
-
-
-
-
+        print("This is the highest amount of money you should of had: %s." % MaxMoney)
+        print("and the round you should have had that money: %s." % MaxMoneyRound)
