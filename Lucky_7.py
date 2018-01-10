635db80af6cc333be2f19b3926552ece5a79019d
Maria <31050501+MariaCloee@users.noreply.github.com>
2018-01-10 15:41:40 -0800

    Finished One Part
    1/10/18

--- Lucky_7.py
+++ Lucky_7.py
@@ -6,6 +6,8 @@ lucky_seven = 7
 rounds = 0
 MaxMoney = 95
 MaxMoneyRound = 20
+HighestMoney = 15
+TheRound = 0
 while Money > 0:
     print("This is your money right now: %s." % Money)
 
@@ -14,15 +16,23 @@ while Money > 0:
     print(Dice1, Dice2)
 
     if Dice1 + Dice2 == lucky_seven:
-        Money += 5
+        Money += 4
+        if Money > HighestMoney:
+            HighestMoney = Money
     elif Dice1 + Dice2 > lucky_seven:
         Money -= 1
+        if Money > HighestMoney:
+            HighestMoney = Money
     elif Dice1 + Dice2 < lucky_seven:
         Money -= 1
+        if Money > HighestMoney:
+            HighestMoney = Money
     print("You now have $" + str(Money) + " left")
     rounds += 1
 
     if Money == 0:
         print("This is how many rounds you have played: %s." % rounds)
-        print("This is the highest amount of money you should of had: %s." % MaxMoney)
-        print("and the round you should have had that money: %s." % MaxMoneyRound)
+        # print("This is the highest amount of money you should of had: %s." % MaxMoney)
+        # print("and the round you should have had that money: %s." % MaxMoneyRound)
+        print("This is the highest amount of money you had: %s." % HighestMoney)
+        print("and this is the round you had that money: %s." % TheRound)
