8ab6356a061c19cc426aaa4eb80f3ac052933385
Maria <31050501+MariaCloee@users.noreply.github.com>
2018-01-11 15:09:45 -0800

    Finish Lucky 7
    1/11/18

--- Lucky_7.py
+++ Lucky_7.py
@@ -1,38 +1,40 @@
 import random
 
-Money = 15
-Bet = 1
+money = 15
+bet = 1
 lucky_seven = 7
 rounds = 0
-MaxMoney = 95
-MaxMoneyRound = 20
-HighestMoney = 15
-TheRound = 0
-while Money > 0:
-    print("This is your money right now: %s." % Money)
+max_money_round = 0
+highest_money = 15
+
+while money > 0:
+    print("This is your money right now: %s." % money)
 
     Dice1 = int(random.randint(1, 6))
     Dice2 = int(random.randint(1, 6))
     print(Dice1, Dice2)
 
     if Dice1 + Dice2 == lucky_seven:
-        Money += 4
-        if Money > HighestMoney:
-            HighestMoney = Money
+        money += 4
+        if money > highest_money:
+            highest_money = money
+            max_money_round = rounds
     elif Dice1 + Dice2 > lucky_seven:
-        Money -= 1
-        if Money > HighestMoney:
-            HighestMoney = Money
+        money -= 1
+        if money > highest_money:
+            highest_money = money
+            max_money_round = rounds
     elif Dice1 + Dice2 < lucky_seven:
-        Money -= 1
-        if Money > HighestMoney:
-            HighestMoney = Money
-    print("You now have $" + str(Money) + " left")
+        money -= 1
+        if money > highest_money:
+            highest_money = money
+            max_money_round = rounds
+    print("You now have $" + str(money) + " left")
     rounds += 1
 
-    if Money == 0:
+    if money == 0:
         print("This is how many rounds you have played: %s." % rounds)
-        # print("This is the highest amount of money you should of had: %s." % MaxMoney)
-        # print("and the round you should have had that money: %s." % MaxMoneyRound)
-        print("This is the highest amount of money you had: %s." % HighestMoney)
-        print("and this is the round you had that money: %s." % TheRound)
+        # print("This is the highest amount of money you should of had: %s." % Maxmoney)
+        # print("and the round you should have had that money: %s." % maxmoneyround)
+        print("This is the highest amount of money you had: $%s." % highest_money)
+        print("and this is the round you had that money: round %s." % max_money_round)
