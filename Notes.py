52c6707b319f41de6fffc2287666227f5f7cb7e0
Maria <31050501+MariaCloee@users.noreply.github.com>
2018-01-18 15:39:19 -0800

    Lists
    1/18/18

--- Notes.py
+++ Notes.py
@@ -1,129 +1,163 @@
-import random   # imports should be at the top
+# import random   # imports should be at the top
+#
+# print("Hello World")
+#
+# # Maria Garcia
+#
+# print(3+5)
+# print(5-3)
+# print(5*3)
+# print(6/2)
+# print(3**2)   # 2 ** means to the power of and 1 * means multiply
+#
+# print()   # Creates a blank line
+# print("See if you can figure this out")
+# print(100 % 3)
+#
+# # Variable
+# car_name = "Wiebe Mobile"
+# car_type = "Lamborghini Sesto Elemento"
+# car_cylinders = 8
+# car_mpg = 9000.1
+#
+# # Inline Printing
+# print("My car is the %s" % car_name)  # %s = insert and  %d = number
+# print("My car is the %s. It is a %s." % (car_name, car_type))
+#
+# # Taking input
+# name = input("What is your name?")
+# print("Hello %s." % name)
+# # print(name)
+#
+# age = input("What is your age?")
+# print("You are %s years old." % age)
+#
+# # Change the file
+#
+#
+# def print_hw():
+#     print("Hello World")
+#     # 4 spaces = 1 indent (very important)
+#
+#
+# print_hw()
+#
+#
+# def say_hi(name1):  # name is a "parameter"
+#     print("Hello %s." % name1)
+#     print("I hope you have a fantastic day!")
+#
+#
+# say_hi("John")
+#
+#
+# def birthday(age1):
+#     age1 += 1   # age = age + 1
+#     print(age1)
+#
+#
+# say_hi("John")
+# print("John is 15. Next year:")
+# birthday(15)
+#
+# # Press Ctrl-A = Select all and Ctrl-/ = to comment old code
+# # Press Ctrl-A and Ctrl-/ to select everything and comment everything
+#
+#
+# def f(x):
+#     return x**5 + 4 * x**4 - 17*x**2 + 4
+#
+#
+# print(f(3))
+# print(f(3) + f(5))
+#
+# # If statements
+#
+#
+# def grade_calc(percentage):
+#     if percentage >= 90:
+#         return "A"
+#     elif percentage >= 80:  # Else if = elif
+#         return "B"
+#     elif percentage >= 70:
+#         return "C"
+#     elif percentage >= 60:
+#         return "D"
+#     else:
+#         return "F"
+#
+# # Loops
+#
+#
+# for num in range(5):
+#     print(num + 1)
+#
+# for mystery in "Hello World":    # Don't use a blue word as the 'name'
+#     print(mystery)
+#
+# a = 1
+# while a < 10:
+#     print(a)
+#     a += 1
+#
+# response = ""
+# while response != "Hello":  # ! = not
+#     response = input("Say \"Hello\"")   # \\ 2 of these = print the actual \ on the program
+#
+# print("Hello \nWorld")   # \n means newline
+#
+#
+# print(random.randint(0, 6))
+#
+#
+# # Comparisons
+# print(1 == 1)   # Two equal signs to compare
+# print(1 != 2)    # One is not euqal to 2
+# print(not False)    # This prints True
+# print(1 == 1 and 4 <= 5)
+# print(1 < 0 or 5 > 1)
+#
+#
+# # Recasting
+# c = '1'
+# print(c == 1)   # False - C is a String, 1 is an int
+# print(int(c) == 1)    # True - Compares two ints
+# print(c == str(1))    # True - Compares two strings
+#
+#
+# num = input("Give me a number")
+# # Inputs are ALWAYS (!!!!!!!!!) of type string
+#
 
-print("Hello World")
+# Lists
 
-# Maria Garcia
+the_count = [1, 2, 3, 4, 5]
+characters = ["graves", "Dory", "Boots", "Dora", "Shrek", "Obi-wan", "Carl"]
+print(characters[0])
+print(characters[4])
 
-print(3+5)
-print(5-3)
-print(5*3)
-print(6/2)
-print(3**2)   # 2 ** means to the power of and 1 * means multiply
+print(len(characters))   # Gives you the length of the list
 
-print()   # Creates a blank line
-print("See if you can figure this out")
-print(100 % 3)
+#  Going through Lists
+for char in characters:
+    print(char)
+for cou in the_count:
+    print(cou**2)
 
-# Variable
-car_name = "Wiebe Mobile"
-car_type = "Lamborghini Sesto Elemento"
-car_cylinders = 8
-car_mpg = 9000.1
+len(characters)
+range(3)     # Make a list of the numbers from 0 to 2
+range(len(characters))  # Makes a list of ALL INDICES
 
-# Inline Printing
-print("My car is the %s" % car_name)  # %s = insert and  %d = number
-print("My car is the %s. It is a %s." % (car_name, car_type))
+for num in range(len(characters)):
+    char = characters[num]
+    print("The character at index %d is %s." % (num, char))
 
-# Taking input
-name = input("What is your name?")
-print("Hello %s." % name)
-# print(name)
+str1 = "Hello World!"
+listOne = list(str1)
+print(listOne)
+listOne[11] = '.'
+print(listOne)
+newStr = "".join(listOne)
+print(newStr)
 
-age = input("What is your age?")
-print("You are %s years old." % age)
-
-# Change the file
-
-
-def print_hw():
-    print("Hello World")
-    # 4 spaces = 1 indent (very important)
-
-
-print_hw()
-
-
-def say_hi(name1):  # name is a "parameter"
-    print("Hello %s." % name1)
-    print("I hope you have a fantastic day!")
-
-
-say_hi("John")
-
-
-def birthday(age1):
-    age1 += 1   # age = age + 1
-    print(age1)
-
-
-say_hi("John")
-print("John is 15. Next year:")
-birthday(15)
-
-# Press Ctrl-A = Select all and Ctrl-/ = to comment old code
-# Press Ctrl-A and Ctrl-/ to select everything and comment everything
-
-
-def f(x):
-    return x**5 + 4 * x**4 - 17*x**2 + 4
-
-
-print(f(3))
-print(f(3) + f(5))
-
-# If statements
-
-
-def grade_calc(percentage):
-    if percentage >= 90:
-        return "A"
-    elif percentage >= 80:  # Else if = elif
-        return "B"
-    elif percentage >= 70:
-        return "C"
-    elif percentage >= 60:
-        return "D"
-    else:
-        return "F"
-
-# Loops
-
-
-for num in range(5):
-    print(num + 1)
-
-for mystery in "Hello World":    # Don't use a blue word as the 'name'
-    print(mystery)
-
-a = 1
-while a < 10:
-    print(a)
-    a += 1
-
-response = ""
-while response != "Hello":  # ! = not
-    response = input("Say \"Hello\"")   # \\ 2 of these = print the actual \ on the program
-
-print("Hello \nWorld")   # \n means newline
-
-
-print(random.randint(0, 6))
-
-
-# Comparisons
-print(1 == 1)   # Two equal signs to compare
-print(1 != 2)    # One is not euqal to 2
-print(not False)    # This prints True
-print(1 == 1 and 4 <= 5)
-print(1 < 0 or 5 > 1)
-
-
-# Recasting
-c = '1'
-print(c == 1)   # False - C is a String, 1 is an int
-print(int(c) == 1)    # True - Compares two ints
-print(c == str(1))    # True - Compares two strings
-
-
-num = input("Give me a number")
-# Inputs are ALWAYS (!!!!!!!!!) of type string
+#
