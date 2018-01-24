99ae3072fe126e451bcf22476115901d21ad03c3
Maria <31050501+MariaCloee@users.noreply.github.com>
2018-01-24 15:44:36 -0800

    Finished Hangman
    1/24/18

--- Maria Garcia - Hangman.py
+++ Maria Garcia - Hangman.py
@@ -13,32 +13,47 @@ import string
 
 letters_guessed = []
 turns_left = 10
-word_bank = ["seal", "puppy", "hamster", "horse", "monkey", "dog", "unicorn", "red wolf", "duck", "whales", "cat"]
+word_bank = ["Seal", "Puppy", "Hamster", "Horse", "Monkey", "Dog", "Unicorn", "Red wolf", "Duck", "Whales", "Cat"]
 a_word = random.randint(0, 10)
 chosen = word_bank[a_word]
-the_right_guess = (random.choices(word_bank))
+points = 0
 
-list_one = list(chosen)
-print(list_one)
+correct_letters = list(chosen)
 letters = string.ascii_letters
 
 # each turn
+print("You have 10 tries to guess this word(might includes spaces and uppercase letter(s)) Good Luck. ")
+print("If you can't do it, type 'quit' to stop.")
 
-# print(''.join(list_one))  # prints out the list for the user
-# output =
 while turns_left > 0:
     list_form = []
 
-    # print(letters_guessed)
     for letter in chosen:
-        if letter in letters_guessed:
+        if letter.lower() in letters_guessed:
             # Show the letter
             list_form.append(letter)
         else:
             # Hide the letter
             list_form.append("*")
-    print(list_form)
-    guess = input("Guess a letter")
+
+    new_str = ''.join(list_form)
+    print(new_str)
+
+    if list_form == correct_letters:
+        print("Good Job! You guessed it!")
+        exit(0)
+    print("You have %d points." % points)
+    guess = input("Guess a letter ").lower()
     if guess == 'quit':
         exit(0)
     letters_guessed.append(guess)
+    if guess in chosen:
+        points += 1
+        print("YAY! You got it right!")
+    else:
+        turns_left -= 1
+        print("Sorry, but that was wrong!")
+
+if turns_left == 0:
+    print("Good job, but the word was %s." % chosen)
+    print("You ended with %d points." % points)
