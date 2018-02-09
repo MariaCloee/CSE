9c683fede1a0b2a4cceb95155d6b8ef9731c56ae
MariaCloee <31050501+MariaCloee@users.noreply.github.com>
2018-02-09 15:40:30 -0800

    Started Descriptions
    2/9/18

--- Dictionary_World_Map-Maria_Garcia.py
+++ Dictionary_World_Map-Maria_Garcia.py
@@ -1,31 +1,38 @@
 world_map = {
-    'WESTHOUSE': {
-        'NAME': 'WEST OF HOUSE',
-        'DESCRIPTION': 'You are west of a small house.',
+    'MOANA_HOUSE': {
+        'NAME': "Moana's House",
+        'DESCRIPTION': 'This place is where Moana lives with her family and there are 4 exits: to the west there is'
+                       'a path, to the east is where grandma lives, north is the ocean shore, and south is a path.',
         'PATHS': {
-            'NORTH': 'NORTHHOUSE',
-            'SOUTH': 'SOUTHHOUSE'
+            'NORTH': 'OCEAN_SHORE',
+            'SOUTH': 'CHIEF_STONES',
+            'WEST': 'PALM_TREES',
+            'EAST': 'GRANDMA_HOME'
+
         }
     },
-    'NORTHHOUSE': {
-        'NAME': 'NORTH OF HOUSE',
-        'DESCRIPTION': 'Insert description here',
+    'CHIEF_STONES': {
+        'NAME': 'Chief’s stones mountain',
+        'DESCRIPTION': 'You have arrived to a sacred place only for chiefs. '
+                       'There is only a pile of stones and paths to the north and northwest and northeast.',
         'PATHS': {
-            'WEST': 'WESTHOUSE',
+            'NORTH': 'MOANA_HOUSE',
+            'SOUTH': 'CHIEF_STONES',
+            'NORTHWEST': 'PALM_TREES',
+            'NORTHEAST': 'GRANDMA_HOME'
         }
     },
-    'SOUTHHOUSE':  {
-        'NAME': 'SOUTH OF HOUSE',
-        'DESCRIPTION': 'Insert description here.',
+    'GRANDMA_HOUSE':  {
+        'NAME': 'Grandma’s home',
+        'DESCRIPTION': 'Grandma lives and there are 3 exits: west, southwest and east.',
         'PATHS': {
-            'WEST': 'WESTHOUSE'
+            'WEST': 'MOANA_HOUSE',
+            'EAST': 'VILLAGER_HOMES'
         }
     }
 }
 
 current_node = world_map['WESTHOUSE']
 print(current_node['DESCRIPTION'])
-
 # 15 rooms at least
 # Have title, descriptions, and paths
-
