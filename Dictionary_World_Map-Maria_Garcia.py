86b9757c0699c5819c1f01625b6485da83815806
MariaCloee <31050501+MariaCloee@users.noreply.github.com>
2018-02-13 15:40:47 -0800

    Description
    2/13/18

--- Dictionary_World_Map-Maria_Garcia.py
+++ Dictionary_World_Map-Maria_Garcia.py
@@ -29,10 +29,84 @@ world_map = {
             'WEST': 'MOANA_HOUSE',
             'EAST': 'VILLAGER_HOMES'
         }
-    }
+    },
+    'VILLAGER_HOMES': {
+        'NAME': "Villagers' Homes",
+        'DESCRIPTION': 'This is where all the villagers sleep and play. There are 2 paths: west and northwest.',
+        'PATHS': {
+            'WEST': 'GRANDMA_HOUSE',
+            'NORTHWEST': 'FISHING_AREA'
+        }
+    },
+    'PALM_TREES': {
+        'NAME': "Palm Trees",
+        'DESCRIPTION': 'You are surrounded by palm trees with coconuts. There is a tree to your left that has a low '
+                       'branch and only one coconut. There are 4 paths: west to ocean shore, east is a path, southeast '
+                       'is a path up the mountain, and north is a block by rocks.',
+        'PATHS': {
+            'WEST': 'OTHER_OCEAN_SHORE',
+            'EAST': 'MOANA_HOUSE',
+            'SOUTHEAST': 'CHIEF_STONES'
+        }
+    },
+    'OTHER_OCEAN_SHORE': {
+        'NAME': "The Other Ocean Shore",
+        'DESCRIPTION': ' It is other ocean shore with the waterfall to the north and a path to the east.',
+        'PATHS': {
+            'NORTH': 'WATERFALL',
+            'EAST': 'PALM TREES'
+        }
+    },
+    'WATERFALL': {
+        'NAME': "Waterfall",
+        'DESCRIPTION': ' There is a waterfall leading to the other ocean shore to the south.  '
+                       'Also, there is a path back east. Also, there are some boats. ',
+        'PATHS': {
+            'SOUTH': 'OTHER_OCEAN_SHORE',
+            'EAST': 'HIDDEN_CAVE'
+        }
+    },
+    'HIDDEN_CAVE': {
+        'NAME': "The Hidden Cave",
+        'DESCRIPTION': 'You found the hidden cave. There is only 2 exits: back to the east and to the west. ',
+        'PATHS': {
+            'WEST': 'WATERFALL',
+            'EAST': 'OCEAN_SHORE'
+        }
+    },
+    'OCEAN_SHORE': {
+        'NAME': "Ocean Shore",
+        'DESCRIPTION': 'You are in the middle of the ocean. There are 4 directions: west, north, east, and back south.'
+                       'I suggested to not go east until you find Mali. ',
+        'PATHS': {
+            'WEST': 'HIDDEN_CAVE',
+            'EAST': 'FISHING_AREA',
+            'SOUTH': "MOANA_HOUSE",
+            'NORTH': 'INTO_OCEAN'
+        }
+    },
+    'FISHING_AREA': {
+        'NAME': "Fishing Area",
+        'DESCRIPTION': 'This is where there are fishing nets and 2 paths to the west and to the east. '
+                       'Also, there are some boats with paddles.',
+        'PATHS': {
+            'WEST': 'WATERFALL',
+            'SOUTHEAST': "VILLAGER_HOMES"
+        }
+    },
+    'INTO_OCEAN': {
+        'NAME': "Into the Ocean",
+        'DESCRIPTION': 'This is where there are fishing nets and 2 paths to the west and to the east. '
+                       'Also, there are some boats with paddles.',
+        'PATHS': {
+            'WEST': 'WATERFALL',
+            'SOUTHEAST': "VILLAGER_HOMES"
+        }
+    },
+
 }
 
-current_node = world_map['WESTHOUSE']
-print(current_node['DESCRIPTION'])
+# current_node = world_map['WESTHOUSE']
+# print(current_node['DESCRIPTION'])
 # 15 rooms at least
 # Have title, descriptions, and paths
